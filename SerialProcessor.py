5a2006eb9eedd0074892d50d01476309946489a6
mitchellss <stephen.mitchell2299@gmail.com>
2021-03-10 12:39:06 -0500

    round time to nearest minute

--- SerialProcessor.py
+++ SerialProcessor.py
@@ -49,14 +49,14 @@ class SerialProcessor:
         self.sr.reset_input_buffer()
         print('Reading Data')
         index = count()
-        startTime = time.time()
         self.sr.readline()
         while self.is_running:
             file = open(self.csvname + '.csv', 'a')
             # data_line = self.sr.readline().decode('utf-8')
             sr_bytes = self.sr.readline()
             decoded_bytes = sr_bytes[0:len(sr_bytes) - 2].decode('utf-8')
-            line = str(next(index)) + ', ' + str(time.time()) + ', ' + str(decoded_bytes)
+            record_time = time.time()
+            line = str(next(index)) + ', ' + str(record_time - record_time % 60) + ', ' + str(decoded_bytes)
 
             # if line looks like accelerometer data, add it to the queue. if not, just print it out.
             if len(decoded_bytes) > 4:
