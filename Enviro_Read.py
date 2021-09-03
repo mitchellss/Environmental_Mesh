a44b83be0ea8ea31393e660d0e8d1ff401f6b98a
Tyler <tylerwebster206@gmail.com>
2021-09-03 12:33:11 -0400

    "Python conversion of serial data to dict"

--- Enviro_Read.py
+++ Enviro_Read.py
@@ -2,6 +2,7 @@ from SerialProcessor import SerialProcessor
 from multiprocessing import Process, Queue
 import sys
 import json
+import ast
 
 if len(sys.argv) == 1:
     print("Usage:\n python Enviro_Read.py <Port>")
@@ -10,6 +11,15 @@ if len(sys.argv) == 1:
 sp = SerialProcessor(str(sys.argv[1]), 9600, 'test')
 sp.go()
 while sp.is_running:
-    line = "\'" + str(sp.queue.get()) + "\'"
-    json_derulo = json.loads(eval(line))
-    print(json_derulo["406908213773201128"])
+    line =  str(sp.queue.get())
+    if "{" in line:
+        try:
+            line_dict = ast.literal_eval(line)
+            print(line_dict)
+        except:
+            print("fail")
+    else:
+        print(line)
+    #json_derulo = json.loads(eval(line))
+    #print(json_derulo["406908213773201128"])
+sp.quit()
